4f120badb296824fbdeff0b2af992e93895930d9
Alexander Schelchere <ascellie@gmail.com>
2019-06-06 10:24:20 -0400

    using them in run

--- run.rb
+++ run.rb
@@ -1,2 +1,16 @@
+require "pry"
+
 require_relative './models/tweet.rb'
 require_relative './models/user.rb'
+
+user1 = User.new("coffee_dad")
+user2 = User.new("wacky_the_clown")
+
+tweet1 = Tweet.new("Need Coffee", user1)
+tweet2 = Tweet.new("need money...", user2)
+tweet3 = Tweet.new("I'll show them all...", user1)
+
+
+binding.pry
+
+0
