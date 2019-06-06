369bda652dd55f7b695232512418b1e6e7b44427
Alexander Schelchere <ascellie@gmail.com>
2019-06-06 10:12:59 -0400

    user skeleton

---
+++ models/user.rb
@@ -0,0 +1,17 @@
+class User
+    attr_reader :username
+
+    def initialize(username)
+        @username = username
+    end
+
+    def tweets
+        #save for later
+    end
+
+    def post_tweets
+        #save for later
+    end
+
+end
+
